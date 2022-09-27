1aa2c160e6c6a3ad00c5aa81414a99e5b12d8b64
Lucas Barbosa <contato@lucasbarbosa.dev>
2022-09-27 06:46:00 -0300

    Revert ":heavy_plus_sign: Instalação de styled-components"
    This reverts commit 21016abf70a5b4928d960b9695bee7eb7f5f16b1.

--- App.tsx
+++ App.tsx
@@ -1,6 +1,21 @@
-import React from "react";
-import { Dashboard } from "./src/screens/Dashboard";
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
-  return <Dashboard />;
+  return (
+    <View style={styles.container}>
+      <Text>Open up App.js to start working on your app!</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
