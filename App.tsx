ba4f53e265171b5d75641730c633bd2aa5d26a9a
Lucas Barbosa <lukebarbos@gmail.com>
2022-09-28 13:54:15 -0300

    :sparkles: HighlightCards component

--- App.tsx
+++ App.tsx
@@ -11,6 +11,7 @@ import {
 
 import theme from "./src/global/styles/theme";
 import { Dashboard } from "./src/screens/Dashboard";
+import { StatusBar } from "expo-status-bar";
 
 export default function App() {
   const [fontsLoaded] = useFonts({
@@ -25,6 +26,7 @@ export default function App() {
 
   return (
     <ThemeProvider theme={theme}>
+      <StatusBar backgroundColor={theme.colors.primary} style="light" />
       <Dashboard />
     </ThemeProvider>
   );
--- src/components/HighlightCard/styles.ts
+++ src/components/HighlightCard/styles.ts
@@ -9,6 +9,7 @@ export const Container = styled.View`
   border-radius: 5px;
   padding: 19px 23px;
   padding-bottom: ${RFValue(42)}px;
+  margin-right: 16px;
 `;
 
 export const Header = styled.View`
@@ -16,12 +17,30 @@ export const Header = styled.View`
   justify-content: space-between;
 `;
 
-export const Title = styled.Text``;
+export const Title = styled.Text`
+  font-family: ${({ theme }) => theme.fonts.regular};
+  font-size: ${RFValue(14)}px;
 
-export const Icon = styled(Feather)``;
+  color: ${({ theme }) => theme.colors.text_dark};
+`;
+
+export const Icon = styled(Feather)`
+  font-size: ${RFValue(40)}px;
+`;
 
 export const Footer = styled.View``;
 
-export const Amount = styled.Text``;
+export const Amount = styled.Text`
+  font-family: ${({ theme }) => theme.fonts.medium};
+  font-size: ${RFValue(32)}px;
 
-export const LastTransaction = styled.Text``;
+  color: ${({ theme }) => theme.colors.text_dark};
+  margin-top: 32px;
+`;
+
+export const LastTransaction = styled.Text`
+  font-family: ${({ theme }) => theme.fonts.regular};
+  font-size: ${RFValue(12)}px;
+
+  color: ${({ theme }) => theme.colors.text};
+`;
